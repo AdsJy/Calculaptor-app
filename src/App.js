13bf391c142fe8e8bb8b38e93f034eea37b863ba
Adisu Assefa <adisuassefa301@gmail.com>
2024-03-25 11:16:08 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from 'react';
+import Button from './Button';
+import Display from './Display';
 
-function App() {
+const App = () => {
+  const [displayValue, setDisplayValue] = useState('');
+
+  const handleClick = (label) => {
+    if (label === '=') {
+      try {
+        setDisplayValue(eval(displayValue).toString());
+      } catch (error) {
+        setDisplayValue('Error');
+      }
+    } else if (label === 'C') {
+      setDisplayValue('');
+    } else {
+      setDisplayValue(displayValue + label);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='calculator'>
+      <Display value={displayValue} />
+      <div className='button-container'>
+        <Button label="7" onClick={handleClick} />
+        <Button label="8" onClick={handleClick} />
+        <Button label="9" onClick={handleClick} />
+        <Button label="/" onClick={handleClick} />
+      </div>
+      <div className='button-container'>
+        <Button label="4" onClick={handleClick} />
+        <Button label="5" onClick={handleClick} />
+        <Button label="6" onClick={handleClick} />
+        <Button label="*" onClick={handleClick} />
+      </div>
+      <div className='button-container'>
+        <Button label="1" onClick={handleClick} />
+        <Button label="2" onClick={handleClick} />
+        <Button label="3" onClick={handleClick} />
+        <Button label="-" onClick={handleClick} />
+      </div>
+      <div className='button-container'>
+        <Button label="0" onClick={handleClick} />
+        <Button label="C" onClick={handleClick} />
+        <Button label="=" onClick={handleClick} />
+        <Button label="+" onClick={handleClick} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
